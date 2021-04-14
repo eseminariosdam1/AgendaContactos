e86ac56f1aded1dda5b7160a2359034f4c450213
eseminariosdam1 <eseminariosdam1@gmail.com>
2021-04-14 20:02:37 +0200

    Metodo felicitar de la clase AgendaContacto terminado (comentarios añadidos en metodos anteriores)

--- src/AgendaContactos.java
+++ src/AgendaContactos.java
@@ -29,7 +29,10 @@ public class AgendaContactos {
 
 		return null;
 	}
-
+	/*
+	 * Devuelve una colección ArrayList con todos los contactos (personales o
+	 * profesionales) que incluyen el texto indicado en su nombre o apellidos.
+	 */
 	public List<Contacto> buscarContactos(String texto) {
 		List<Contacto> contactosEncontrados = new ArrayList<Contacto>();
 		for (Map.Entry<Character, Set<Contacto>> entradaAgenda : agenda.entrySet()) {
@@ -43,14 +46,44 @@ public class AgendaContactos {
 		return contactosEncontrados;
 	}
 
+	/*
+	 * Dada una letra devuelve una colección ArrayList con los contactos personales
+	 * en esa letra. Si la letra no está en la agenda se devuelve null.
+	 */
 	public List<Personal> personalesEnLetra(char letra) {
+		List<Personal> personalesEncontrados = new ArrayList<Personal>();
+		for (Map.Entry<Character, Set<Contacto>> entradaAgenda : agenda.entrySet()) {
+			if (entradaAgenda.getKey() == letra) {
+				for (Contacto contacto : entradaAgenda.getValue()) {
+					if (contacto.getClass() == Personal.class) {
+						if (contacto.getPrimeraLetra() == letra) {
+							personalesEncontrados.add((Personal) contacto);
+						}
+					}
+				}
+			}
 
-		return null;
+		}
+
+		return personalesEncontrados;
 	}
-
+	/*
+	 * Devuelve una colección ArrayList con todos los contactos personales a los que
+	 * hay que felicitar.
+	 */
 	public List<Personal> felicitar() {
+		List<Personal> personalesAFelicitar = new ArrayList<Personal>();
+		for (Map.Entry<Character, Set<Contacto>> entradaAgenda : agenda.entrySet()) {
+			for (Contacto contacto : entradaAgenda.getValue()) {
+				if (contacto.getClass() == Personal.class) {
+					if (((Personal) contacto).esCumpleaños()) {
+						personalesAFelicitar.add((Personal) contacto);
+					}
+				}
+			}
 
-		return null;
+		}
+		return personalesAFelicitar;
 	}
 
 	public void personalesPorRelacion() {
