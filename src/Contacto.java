9ac331bb25cafe21116ac571df3ff1532c946c0b
npetrovhdam1 <npetrovhdam1@gmail.com>
2021-04-14 18:31:01 +0200

    Clase Contacto completada

--- src/Contacto.java
+++ src/Contacto.java
@@ -1,4 +1,7 @@
-public class Contacto {
+//@Autor: Eenko Seminario  y  Nikolay Petrov
+
+public abstract class Contacto implements Comparable<Contacto>
+{
 	private String nombre;
 	private String apellidos;
 	private String telefono;
@@ -49,5 +52,66 @@ public class Contacto {
 		return email.hashCode();
 
 	}
-
+	
+	
+	public char getPrimeraLetra() 
+	{
+		return apellidos.charAt (0);
+	}
+	
+	
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Contacto other = (Contacto) obj;
+		if (apellidos == null) {
+			if (other.apellidos != null)
+				return false;
+		} else if (!apellidos.equals(other.apellidos))
+			return false;
+		if (email == null) {
+			if (other.email != null)
+				return false;
+		} else if (!email.equals(other.email))
+			return false;
+		if (nombre == null) {
+			if (other.nombre != null)
+				return false;
+		} else if (!nombre.equals(other.nombre))
+			return false;
+		if (telefono == null) {
+			if (other.telefono != null)
+				return false;
+		} else if (!telefono.equals(other.telefono))
+			return false;
+		return true;
+	}
+	
+	 
+	public int compareTo(Contacto contactoA) 
+	{
+		int Comparador = apellidos.compareTo(contactoA.getApellidos() );
+	
+		if (Comparador == 0 ) 
+		{
+			Comparador = nombre.compareTo(contactoA.getNombre());
+		}
+		return Comparador;	
+	}
+		
+	
+	public abstract String getFirmaEmail();
+	
+	
+	@Override
+	public String toString() {
+		return "Contacto [nombre=" + nombre + ", apellidos=" + apellidos + ", telefono=" + telefono + ", email=" + email
+				+ "]";
+	}
+	
 }
