569cdfce7d8f81c9e229d1b3300d1154755916d2
eseminariosdam1 <eseminariosdam1@gmail.com>
2021-04-16 21:42:32 +0200

    PROYECTO TERMINADO

--- src/ut7/agenda/modelo/AgendaContactos.java
+++ src/ut7/agenda/modelo/AgendaContactos.java
@@ -1,6 +1,6 @@
 package ut7.agenda.modelo;
 
-//@author Eneko Seminario y Nikolay Petrov
+//@author Eneko Seminario y Nikolay Petrov.
 
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
