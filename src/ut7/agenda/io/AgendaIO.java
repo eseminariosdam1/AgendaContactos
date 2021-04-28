d9b5fa61051fd63114c9d10f81ea93353fe2d70b
eseminariosdam1 <eseminariosdam1@gmail.com>
2021-04-28 18:34:59 +0200

    modificado metodo importar añadiendo un parametro de entrada con el nombre del fichero a leer, capturo la excepcion al parsear la linea y devuelvo el numero de errores. se elimina el parseo a localDate de la fecha de nacimiento en la fucnion parsearLinea.

--- src/ut7/agenda/io/AgendaIO.java
+++ src/ut7/agenda/io/AgendaIO.java
@@ -1,4 +1,5 @@
 package ut7.agenda.io;
+
 //@author Eneko Seminario y Nikolay Petrov
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
@@ -10,82 +11,53 @@ import ut7.agenda.modelo.Personal;
 import ut7.agenda.modelo.Profesional;
 import ut7.agenda.modelo.Relacion;
 
-
-
 /**
  * Utilidades para cargar la agenda
  */
 public class AgendaIO {
 
-	public static void importar(AgendaContactos agenda) {
-		Scanner input = new Scanner(agenda.getClass().getResourceAsStream("/agenda.csv"));
+	public static int importar(AgendaContactos agenda, String fichero) {
+		int errores = 0;
+		Scanner input = new Scanner(agenda.getClass().getResourceAsStream(fichero));
 		while (input.hasNextLine()) {
 			String linea = input.nextLine();
-			Contacto contacto = parsearLinea(linea);
-			agenda.anadirContacto(contacto);
+			try {
+				Contacto contacto = parsearLinea(linea);
+				agenda.anadirContacto(contacto);
+			} catch (Exception e) {
+				errores++;
+			}
+
 		}
 		input.close();
-
+		return errores;
 	}
 
-private static Contacto parsearLinea(String linea) { 
-		
-		String[] partes = linea.split(","); 
-       
-		int tipoContacto = Integer.parseInt(partes[0].trim()); 
-		
-		String nombre = partes[1].trim();         
-		String apellidos = partes[2].trim();         
-		String telefono = partes[3].trim();         
-		String email = partes[4].trim();         
-		String empresa; 
-		LocalDate fechaNacimiento;         
-		Relacion relacion;    
-		
-			switch (tipoContacto) {         
-				case 1:             
-					empresa = partes[5].trim();             
-					return new Profesional(nombre, apellidos, telefono, email, empresa);   
-			
-				case 2:             
-					fechaNacimiento =LocalDate.parse(partes[5].trim(), DateTimeFormatter.ofPattern("dd/MM/yyyy"));             
-					relacion = Relacion.valueOf(partes[6].trim());             
-					return new Personal (nombre, apellidos, telefono, email, fechaNacimiento, relacion);         
-			}         
-		return null;     }
-		
+	private static Contacto parsearLinea(String linea) {
 
-	/**
-	 * 
-	 * @return un array de String con todas las líneas de información de todos
-	 *         los contactos. 1 significa contacto profesional, 2 significa
-	 *         contacto personal
-	 */
-	private static String[] obtenerLineasDatos() {
-		return new String[] {
-				"1, Isabel, Acosta Mendioroz,  678895433 ,  iacostamen@gmail.com ,  walden estrella ",
-				"2,  pedro , urruti tello , 616789654 ,  urrutitello@gmail.com , 09/03/2007, amigos",
-				"1, Angel , Esteban Grande , 674544123 ,  aestebang@gmail.com ,  magma publicidad ",
-				"2, elena , bueno ganuza , 6786547699 ,  ebuenogan@gmail.com , 17/03/2000, amigos",
-				"2, amaia , romero sein , 642222343 ,  aromerosein@gmail.com , 09/03/2012, pareja",
-				"2, Ignacio ,  Anto roth ,  688912799 , iantoroth@gmail.com ,  11/11/1969 , padre",
-				"1,  Isabel ,  Acosta Marin , 678895433 ,  iacostamar@gmail.com ,  publicidad holdings ",
-				"1 ,    roberto , casas maura , 666777888 ,  rocasasma@gmail.com ,  strato banca ",
-				"1,juan maria, garcía oliva, 699898111, jmgarcioliva@gmail.com, conway motor ",
-				"2, pedro , urruti tello , 616789654 ,  urrutitello@gmail.com , 17/03/2000, amigos",
-				"1,marta, sanz iris, 622999876, msanzi@gmail.com, jump literatura ",
-				"1,javier, porto luque, 691256777 , japorlu@gmail.com, gas natural ",
-				"1,pablo, ponce larraoz, 689123456, pabloponce@gmail.com, la caixa",
-				"1, javier, marin lancho, 666666666, jruizlanchoe@gmail.com, bbva",
-				"1,juan maria, garcía oliva, 699898111, jmgarcioliva@gmail.com, conway motor ",
-				"2, Berta ,  andia solano ,  621123345 , bandiasol@gmail.com ,  12/12/1999 ,  HIJA",
-				"2, Ignacio ,  Anto roth ,  688912799 , iantoroth@gmail.com ,  11/11/1969 , padre",
-				"  1,  roberto , casas maura , 666777888 ,  rocasasma@gmail.com ,  strato banca ",
-				" 2, daniel , martin martin , 678901234 ,  damrtinmartin@gmail.com , 15/07/1980, amigos",
-				"  2, pablo , martin abradelo , 667788899 ,  martinabra@gmail.com , 31/01/2010, amigos",
-				"  2, susana , santaolalla bilbao , 676767676 ,  ssantaolalla@gmail.com , 17/03/1998, amigos",
-				"  2, adur ,  martin merino ,  611112113 , adurmartinme@gmail.com ,  14/02/2003 , amigos" };
+		String[] partes = linea.split(",");
 
+		int tipoContacto = Integer.parseInt(partes[0].trim());
+
+		String nombre = partes[1].trim();
+		String apellidos = partes[2].trim();
+		String telefono = partes[3].trim();
+		String email = partes[4].trim();
+		String empresa;
+		String fechaNacimiento;
+		Relacion relacion;
+
+		switch (tipoContacto) {
+		case 1:
+			empresa = partes[5].trim();
+			return new Profesional(nombre, apellidos, telefono, email, empresa);
+
+		case 2:
+			fechaNacimiento =partes[5].trim();
+			relacion = Relacion.valueOf(partes[6].trim());
+			return new Personal(nombre, apellidos, telefono, email, fechaNacimiento, relacion);
+		}
+		return null;
 	}
 
 }
