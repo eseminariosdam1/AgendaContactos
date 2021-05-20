3ca4307c6274c621e53f84a445f612110fbcdb80
eseminariosdam1 <eseminariosdam1@gmail.com>
2021-05-20 17:46:18 +0200

    Interfaz Creada

--- src/agenda/interfaz/GuiAgenda.java
+++ src/agenda/interfaz/GuiAgenda.java
@@ -13,12 +13,14 @@ import javafx.scene.control.RadioButton;
 import javafx.scene.control.SeparatorMenuItem;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
+import javafx.scene.control.ToggleGroup;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyCodeCombination;
 import javafx.scene.input.KeyCombination;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.VBox;
+import javafx.scene.text.TextAlignment;
 import javafx.stage.Stage;
 
 public class GuiAgenda extends Application {
@@ -83,72 +85,94 @@ public class GuiAgenda extends Application {
 	private VBox crearPanelBotones() {
 		// a completar
 		VBox panel = new VBox(10);
-		panel.setPadding(new Insets(5));
+		panel.setPadding(new Insets(10));
+		panel.setSpacing(10);
+		BorderPane bordeBuscar = new BorderPane();
 		TextField txtBuscar = new TextField();
+		BorderPane.setMargin(txtBuscar, new Insets(0, 0, 40, 0));
 
 		txtBuscar.setPromptText("Buscar");
+		txtBuscar.setMinHeight(40);
 		txtBuscar.getStyleClass().add("text-field");
 		txtBuscar.setOnAction(event -> {
 			System.out.println();
 		});
-		panel.getChildren().add(txtBuscar);
+		bordeBuscar.setTop(txtBuscar);
+		ToggleGroup grupo = new ToggleGroup();
 		RadioButton radioListarAgenda = new RadioButton("Listar toda la agenda");
+		radioListarAgenda.setSelected(true);
 		radioListarAgenda.getStyleClass().add("radio-button");
 		radioListarAgenda.setOnAction(event -> {
-			System.out.println();
+		
 		});
-		panel.getChildren().add(radioListarAgenda);
+		radioListarAgenda.setToggleGroup(grupo);
 		RadioButton radioListarContactos = new RadioButton("Listar nº contactos");
 		radioListarContactos.getStyleClass().add("radio-button");
 		radioListarContactos.setOnAction(event -> {
 			System.out.println();
 		});
-		panel.getChildren().add(radioListarContactos);
+		radioListarContactos.setToggleGroup(grupo);
 		Button botonListar = new Button("Listar");
-
+		botonListar.setPrefWidth(250);
 		botonListar.getStyleClass().add("botones");
 		botonListar.setOnAction(event -> {
 			System.out.println();
 		});
 
-		panel.getChildren().add(botonListar);
+		BorderPane bordeListar = new BorderPane();
+		BorderPane.setMargin(botonListar, new Insets(0,0,40,0));
+		bordeListar.setTop(botonListar);
 		Button botonEnLetra = new Button("Contactos Personales en letra");
+		botonEnLetra.setPrefWidth(250);
 		botonEnLetra.getStyleClass().add("botones");
 		botonEnLetra.setOnAction(event -> {
 			System.out.println();
 		});
 
-		panel.getChildren().add(botonEnLetra);
+		
 		Button botonOrdenados = new Button("Contactos personales ordenados por fecha");
+		botonOrdenados.setPrefWidth(250);
+		botonOrdenados.setWrapText(true);;
 		botonOrdenados.getStyleClass().add("botones");
+		botonOrdenados.setTextAlignment(TextAlignment.CENTER);
 		botonOrdenados.setOnAction(event -> {
 			System.out.println();
 		});
 
-		panel.getChildren().add(botonOrdenados);
 		Button botonclear = new Button("Clear");
+		botonclear.setPrefWidth(250);
 		botonclear.getStyleClass().add("botones");
 		botonclear.setOnAction(event -> {
 			clear();
 		});
-
-		panel.getChildren().add(botonclear);
+		
+		BorderPane bordeClear = new BorderPane();
+		BorderPane.setMargin(botonclear, new Insets(40,0,0,0));
+		bordeClear.setTop(botonclear);
 		Button botonSalir = new Button("Salir");
+		botonSalir.setPrefWidth(250);
 		botonSalir.getStyleClass().add("botones");
 		botonSalir.setOnAction(event -> {
 			salir();
 		});
 
+		
+		panel.getChildren().add(bordeBuscar);
+		panel.getChildren().add(radioListarAgenda);
+		panel.getChildren().add(radioListarContactos);
+		panel.getChildren().add(bordeListar);
+		panel.getChildren().add(botonEnLetra);
+		panel.getChildren().add(botonOrdenados);
+		panel.getChildren().add(bordeClear);
 		panel.getChildren().add(botonSalir);
-
 		return panel;
 	}
 
 	private GridPane crearPanelLetras() {
 		// a completar
 		GridPane panel = new GridPane();
-		panel.setHgap(10);
-		panel.setVgap(10);
+		panel.setHgap(5);
+		panel.setVgap(5);
 		panel.setPadding(new Insets(10));
 		String abecedario = "ABCDEFGHIJKLMNÑOPQRSTUVWXYZ";
 		int columna = 0;
@@ -161,7 +185,7 @@ public class GuiAgenda extends Application {
 			boton.setOnAction(event -> {
 				System.out.println(letra);
 			});
-			if (columna == 15) {
+			if (columna > abecedario.length() / 2) {
 				columna = 0;
 				fila++;
 			}
